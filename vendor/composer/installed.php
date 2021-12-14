2b430fbbf8e4799275947c65ba79f3442f6ebebb
NOX-0202 <fernandinho.nco@gmail.com>
2021-12-13 23:26:04 -0300

    vendor

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '43b3517f738a2decb1d5e1c659bbb44199124f84',
+        'name' => 'crud/easy',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'coffeecode/datalayer' => array(
+            'pretty_version' => '1.1.4',
+            'version' => '1.1.4.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../coffeecode/datalayer',
+            'aliases' => array(),
+            'reference' => '17fecff8edb8fb5287648875f182acbb80389614',
+            'dev_requirement' => false,
+        ),
+        'crud/easy' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '43b3517f738a2decb1d5e1c659bbb44199124f84',
+            'dev_requirement' => false,
+        ),
+    ),
+);
